7e4d2f9d671ba29464b2f620bb2994141a0fb54b
hamidreza <takloohamidreza@gmail.com>
2022-10-10 17:19:24 +0330

    feat: add function to control poses in board

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const generatePositions = (currentPos) => {
   let x = currentPos[0];
   let y = currentPos[1];
+  // prevent form out of board
+  const check = (x, y) => {
+    if (x > 7 || x < 0 || y > 7 || y < 0) return false;
+    return true;
+  };
 };
